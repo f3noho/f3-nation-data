21d600c4c7a388d687ce5b73fd55acc7907c8e80
Manuel Lopez <jmlopez.rod@gmail.com>
2025-08-31 15:23:38 -0500

    add export

--- f3_nation_data/__init__.py
+++ f3_nation_data/__init__.py
@@ -1,3 +1,4 @@
+from .database import db_session
 from .models import BeatdownRecord, ParsedBeatdown, SqlBeatDownModel
 from .transform import transform_sql_to_beatdown_record
 
@@ -5,5 +6,6 @@ __all__ = [
     'BeatdownRecord',
     'ParsedBeatdown',
     'SqlBeatDownModel',
+    'db_session',
     'transform_sql_to_beatdown_record',
 ]
